22c4d5efb069b1332193619ac05074abaad8ec86
lucas <lucas.chorolqui@gmail.com>
2023-12-29 08:56:38 -0300

    updates

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "TU_CONSUMER_KEY",
+  consumer_secret: "TU_CONSUMER_SECRET",
+  access_token: "TU_ACCESS_TOKEN",
+  access_token_secret: "TU_ACCESS_TOKEN_SECRET",
+};
