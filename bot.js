393d5f9754df6f0bc93ec1ec8f3d5dcf3f8a67e6
lucas <lucas.chorolqui@gmail.com>
2024-01-03 12:48:20 -0300

    updates

--- bot.js
+++ bot.js
@@ -1,11 +1,29 @@
-const rwClient = require("./config");
+const Twitter = require("twitter-v2");
+require("dotenv").config();
 
-const tweet = async () => {
-  try {
-    await rwClient.v1.tweet({ status: "nice" });
-  } catch (e) {
-    console.error(e);
-  }
+const config = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  // bearer_token: process.env.TWITTER_BEARER_TOKEN,
 };
 
-tweet();
+const client = new Twitter(config);
+
+async function postTweet() {
+  const tweetText =
+    "Hello, Twitter! This is my first tweet using the Twitter API.";
+
+  try {
+    const { data } = await client.post("tweets", {
+      status: tweetText,
+    });
+
+    console.log("Tweet posted successfully:", data);
+  } catch (error) {
+    console.error("Error posting tweet:", error);
+  }
+}
+
+postTweet();
--- config.js
+++ config.js
@@ -1,12 +1,13 @@
-const { TwitterApi } = require("twitter-api-v2");
+// const { TwitterApi } = require("twitter-api-v2");
 
-const client = new TwitterApi({
-  apiKey: "t9ngmlO7n7bo0OXON1SXVd1R4",
-  apiSecret: "cLvBofhWTPcxE9xH57jqaNR0GfOEw2TFvY0M7L12K970wlxWyB",
-  accessToken: "863026556146515969-IgRxE092yNzBf2y2R4GpHOovKQ1k3M4",
-  accessSecret: "Vi9dh5iTolrjGgqurK56IYqVCYStGikU6KUzdqUZZdV28",
-});
+// // bearerToken: AAAAAAAAAAAAAAAAAAAAAGcIrwEAAAAA0Me%2Bz5L0kp5WDWwJHi8HGAHnkIk%3D427eXUcXQSO89FFy03SE9H5b106b7ebzHb2iclCIVydCrhtOu3
+// const client = new TwitterApi({
+//   apiKey: "yzhWeo67PdDqj7fQ2Vrk84kkW",
+//   apiSecret: "FJn6fAqjDEzFundIf85PoijaJJfZuLN5twmhXI2s8nNaUwMgZq",
+//   accessToken: "1742521755657244672-Lwk0x1tNSjCky41MgXfWlUlFecy5sk",
+//   accessSecret: "Efrj2Ph1GvilK4cX7zk4MZ8sqZUl2zlGSUMLN2gCujue9",
+// });
 
-const rwClient = client.readWrite;
+// const rwClient = client.readWrite;
 
-module.exports = rwClient;
+// module.exports = rwClient;
